ce2f069bcf353047d93086e483e2e7e01077da71
Antoine Colmard <a.colmard@gmail.com>
2020-04-16 00:34:44 +0200

    :sparkles: Allow zoom on mouse cursor when using mouse wheel

--- src/Renderer.tsx
+++ src/Renderer.tsx
@@ -1,117 +1,22 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef } from "react";
 // @ts-ignore
 // eslint-disable-next-line import/no-webpack-loader-syntax
 import worker from "worker-loader!./RendererWorker";
 import * as Dungeon from "./dungeon";
+import { useViewport } from "./useViewport";
 
 const instance = worker();
 
 export const Renderer: React.FC<{
   dungeon: Dungeon.Dungeon;
 }> = ({ dungeon }) => {
+  // Set the rendered dungeon
+  useEffect(() => instance.postMessage({ type: "SET_DUNGEON", payload: dungeon }), [dungeon]);
+
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
 
-  // Zoom listener
-  const [zoom, setZoom] = useState(1);
-  useEffect(() => {
-    const canvas = canvasRef.current;
-    if (canvas === null) {
-      return undefined;
-    }
-    const listener = (e: WheelEvent) => {
-      if (e.deltaY > 0) {
-        setZoom((zoom) => Math.max(1, zoom - 1));
-      } else {
-        setZoom((zoom) => zoom + 1);
-      }
-    };
-    canvas.addEventListener("wheel", listener);
-    return () => canvas.removeEventListener("wheel", listener);
-  }, []);
-
-  // Panning listeners
-  const [dx, setDx] = useState(0);
-  const [dy, setDy] = useState(0);
-  const start = useRef<{ x: number; y: number } | null>(null);
-  useEffect(() => {
-    const canvas = canvasRef.current;
-    if (canvas === null) {
-      return undefined;
-    }
-    const handleStart = (location: { clientX: number; clientY: number }) => {
-      start.current = { x: location.clientX - dx, y: location.clientY - dy };
-    };
-    const onMouseDownListener = (e: MouseEvent) => {
-      handleStart(e);
-      canvas.style.cursor = "move";
-    };
-    const onTouchStartListener = (e: TouchEvent) => {
-      handleStart(e.touches[0]);
-    };
-    canvas.addEventListener("mousedown", onMouseDownListener);
-    canvas.addEventListener("touchstart", onTouchStartListener);
-    return () => {
-      canvas.removeEventListener("mousedown", onMouseDownListener);
-      canvas.removeEventListener("touchstart", onTouchStartListener);
-    };
-  }, [dx, dy]);
-
-  useEffect(() => {
-    const canvas = canvasRef.current;
-    if (canvas === null) {
-      return undefined;
-    }
-    const onMouseUpListener = () => {
-      start.current = null;
-      canvas.style.cursor = "default";
-    };
-    canvas.addEventListener("mouseup", onMouseUpListener);
-    canvas.addEventListener("mouseleave", onMouseUpListener);
-    canvas.addEventListener("touchend", onMouseUpListener);
-    return () => {
-      canvas.removeEventListener("mouseup", onMouseUpListener);
-      canvas.removeEventListener("mouseleave", onMouseUpListener);
-      canvas.removeEventListener("touchend", onMouseUpListener);
-    };
-  }, []);
-
-  useEffect(() => {
-    const canvas = canvasRef.current;
-    if (canvas === null) {
-      return undefined;
-    }
-    const handleMove = (location: { clientX: number; clientY: number }) => {
-      if (start.current === null) {
-        return;
-      }
-      setDx(location.clientX - start.current.x);
-      setDy(location.clientY - start.current.y);
-    };
-    const onMouseMoveListener = (e: MouseEvent) => {
-      handleMove(e);
-    };
-    const onTouchMoveListener = (e: TouchEvent) => {
-      handleMove(e.touches[0]);
-    };
-    canvas.addEventListener("mousemove", onMouseMoveListener);
-    canvas.addEventListener("touchmove", onTouchMoveListener);
-    return () => {
-      canvas.removeEventListener("mousemove", onMouseMoveListener);
-      canvas.removeEventListener("touchmove", onTouchMoveListener);
-    };
-  }, []);
-
-  useEffect(() => {
-    instance.postMessage({ type: "SET_DPR", payload: window.devicePixelRatio });
-  }, []);
-
-  useEffect(() => {
-    instance.postMessage({ type: "SET_DUNGEON", payload: dungeon });
-  }, [dungeon]);
-
-  useEffect(() => {
-    instance.postMessage({ type: "SET_ZOOM", payload: zoom * window.devicePixelRatio });
-  }, [zoom]);
+  const { zoom, dx, dy } = useViewport(canvasRef);
+  useEffect(() => instance.postMessage({ type: "SET_ZOOM", payload: zoom * window.devicePixelRatio }), [zoom]);
 
   useEffect(() => {
     instance.postMessage({ type: "SET_DX", payload: dx * window.devicePixelRatio });
@@ -121,6 +26,7 @@ export const Renderer: React.FC<{
     instance.postMessage({ type: "SET_DY", payload: dy * window.devicePixelRatio });
   }, [dy]);
 
+  // Worker initialization
   useEffect(() => {
     if (canvasRef.current === null) {
       return;
@@ -132,6 +38,7 @@ export const Renderer: React.FC<{
     };
   }, []);
 
+  // Canvas resize listener
   useEffect(() => {
     const resizeListener = () => {
       if (!canvasRef.current) {
---
+++ src/useViewport.ts
@@ -0,0 +1,99 @@
+import { useCallback, useEffect, useRef, useState } from "react";
+
+const useEventListener = <T extends HTMLElement, K extends keyof HTMLElementEventMap>(
+  type: K,
+  listener: (this: HTMLElement, ev: HTMLElementEventMap[K]) => any,
+  ref: React.RefObject<T | null>,
+  options?: boolean | AddEventListenerOptions
+) => {
+  useEffect(() => {
+    const element = ref.current;
+    if (element === null) {
+      return;
+    }
+    element.addEventListener(type, listener, options);
+    return () => element.removeEventListener(type, listener);
+  }, [type, listener, options, ref]);
+};
+
+export const useViewport = <T extends HTMLElement>(ref: React.RefObject<T | null>) => {
+  const [zoom, setZoom] = useState(1);
+  const [dx, setDx] = useState(0);
+  const [dy, setDy] = useState(0);
+
+  const onMouseWheel = useCallback(
+    (e: WheelEvent) => {
+      const deltaZoom = e.deltaY < 0 ? +1 : -1;
+      const nextZoom = Math.max(1, zoom + deltaZoom);
+      if (nextZoom === zoom) {
+        return;
+      }
+      const mouseX = e.offsetX;
+      const mouseY = e.offsetY;
+      const nextDx = mouseX - (mouseX - dx) * (nextZoom / zoom);
+      const nextDy = mouseY - (mouseY - dy) * (nextZoom / zoom);
+      setZoom(nextZoom);
+      setDx(Math.floor(nextDx));
+      setDy(Math.floor(nextDy));
+    },
+    [dx, dy, zoom]
+  );
+  useEventListener("wheel", onMouseWheel, ref);
+
+  const start = useRef<{ x: number; y: number } | null>(null);
+  // Handle panning start
+  const onPanningStart = useCallback(
+    (e: { clientX: number; clientY: number }) => {
+      start.current = { x: e.clientX - dx, y: e.clientY - dy };
+      const element = ref.current;
+      if (element !== null) {
+        element.style.cursor = "move";
+      }
+    },
+    [dx, dy, ref]
+  );
+  const onTouchPanningStart = useCallback(
+    (e: TouchEvent) => {
+      onPanningStart(e.touches[0]);
+    },
+    [onPanningStart]
+  );
+  useEventListener("mousedown", onPanningStart, ref);
+  useEventListener("touchstart", onTouchPanningStart, ref);
+
+  // Handle panning end
+  const onPanningEnd = useCallback(() => {
+    start.current = null;
+    const element = ref.current;
+    if (element !== null) {
+      element.style.cursor = "default";
+    }
+  }, [ref]);
+  useEventListener("mouseup", onPanningEnd, ref);
+  useEventListener("mouseleave", onPanningEnd, ref);
+  useEventListener("touchend", onPanningEnd, ref);
+  useEventListener("touchcancel", onPanningEnd, ref);
+
+  // Handle element panning
+  const onPanningMove = useCallback((e: { clientX: number; clientY: number }) => {
+    if (start.current === null) {
+      return;
+    }
+    setDx(e.clientX - start.current.x);
+    setDy(e.clientY - start.current.y);
+  }, []);
+  const onTouchPanning = useCallback(
+    (e: TouchEvent) => {
+      onPanningMove(e.touches[0]);
+    },
+    [onPanningMove]
+  );
+  useEventListener("mousemove", onPanningMove, ref);
+  useEventListener("touchmove", onTouchPanning, ref);
+
+  return {
+    zoom,
+    dx,
+    dy,
+  };
+};
